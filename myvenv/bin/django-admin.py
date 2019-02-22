9eb0978c5a47390c45b9cb598dd2b0cb5ecf47a9
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2019-02-21 22:30:45 -0500

    Actually delete local-only folders in master and add .gitignore

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/SJP/Documents/Fourth_Semester/CS3240/project-101-lucky-13/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
