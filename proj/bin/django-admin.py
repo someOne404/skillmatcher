5eecee16d6ee3434f061c2d5bf7fea7c407d2aab
Sung Joon <sungjoon@ridgebacknet.com>
2019-02-25 15:39:16 -0500

    make it better

---
+++ proj/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SJP/Documents/Fourth_Semester/CS3240/project-101-lucky-13/proj/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
