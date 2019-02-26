077eff0d3c6a5ba8d931c4bec583a5abd7c2f738
Bonus <jiayilu96@gmail.com>
2019-02-26 17:09:32 -0500

    Updated the looks of the search page

--- lucky13/settings.py
+++ lucky13/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_match',
     'social_django',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- social_match/urls.py
+++ social_match/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from django.conf.urls import url
 from . import views
+from django_filters.views import FilterView
+from social_match.filters import UserFilter
 
 app_name = 'social_match'
 urlpatterns = [
     path('', views.base, name='base'),
     path('about/', views.about, name='about'),
     path('home/', views.home, name='home'),
-    path('search/', views.search, name='search'),
+    url(r'^search/$', FilterView.as_view(filterset_class=UserFilter,
+        template_name='social_match/search.html'), name='search'),
     path('createpost/', views.createpost, name='createpost'),
     path('profile/', views.profile, name='profile'),
     path('posts/', views.posts, name='posts'),
