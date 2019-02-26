627fc2a47ece1520a23c923a936720f5a2abf619
Bonus <jiayilu96@gmail.com>
2019-02-26 15:48:48 -0500

    Updated searching branch

--- social_match/filters.py
+++ social_match/filters.py
@@ -3,6 +3,12 @@ from .models import User
 import django_filters
 
 class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    last_name = django_filters.CharFilter(lookup_expr='icontains')
+    email = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'phone', 'class_standing', 'graduation_year',]
+        fields = ['first_name', 'last_name', 'email', 'phone', 'class_standing', 'graduation_year',
+                  'majors','minors', 'courses', 'skills', 'interests', 'activities']
+
