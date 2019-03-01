a5a98c5a07e2d202dbe8b41b61d499192285d371
Vivian <vwl4ac@virginia.edu>
2019-02-28 21:37:04 -0500

    wrote tests for inactive functionality

--- social_match/tests.py
+++ social_match/tests.py
@@ -1,25 +1,63 @@
 from django.test import TestCase
 from social_match.models import *
+from social_match.filters import *
 from django.urls import reverse
 
 # Create your tests here.
 class UserModelTest(TestCase):
 
-	def create_test_user(self):
-		return User.objects.create(
-			first_name="Test",
-			last_name="User",
-			phone="+1234567890",
-			class_standing=User.first_year,
-			graduation_year=2019,
-			email='test@virginia.edu',
-		)
+    def create_test_user(self):
+        return User.objects.create(
+            first_name="Test",
+            last_name="User",
+            phone="+1234567890",
+            class_standing=User.first_year,
+            graduation_year=2019,
+            email='test@virginia.edu',
+            username='Test',
+        )
 
-	def test_create_user(self):
-		u = self.create_test_user()
-		self.assertTrue(isinstance(u, User))
+    def create_test_user_inactive(self):
+        return User.objects.create(
+            first_name="Inactive",
+            last_name="User",
+            phone="+1234567890",
+            class_standing=User.first_year,
+            graduation_year=2019,
+            email='inactive@virginia.edu',
+            status_active='False',
+            username='Inactive',
+        )
+
+    def create_test_user_superuser(self):
+        return User.objects.create(
+            first_name="Super",
+            last_name="User",
+            phone="+1234567890",
+            class_standing=User.first_year,
+            graduation_year=2019,
+            email='superuser@virginia.edu',
+            is_superuser='True',
+            username='Superuser',
+        )
+
+    def test_create_user(self):
+        u = self.create_test_user()
+        self.assertTrue(isinstance(u, User))
+
+    def test_inactive_and_superuser_not_in_search_results(self):
+        self.create_test_user()
+        self.create_test_user_inactive()
+        self.create_test_user_superuser()
+
+        GET={'title': 'test'}
+        user_list=User.objects.all()
+        user_filter = UserFilter(GET, queryset=user_list)
+
+        user_list_ideal = User.objects.filter(status_active='True', is_superuser='False')
+        self.assertQuerysetEqual(user_filter.qs, user_list_ideal, transform=lambda x: x)
 
 
 #class SearchingTest(TestCase):
 
-	#def test_first_name(self):
+    #def test_first_name(self):
