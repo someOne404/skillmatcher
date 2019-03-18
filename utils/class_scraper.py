295142a0cd4b073541aaebb42e79d9badd7ca143
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2019-03-18 15:13:29 -0400

    Editing class scraper to only include classes offered in last 4 years. Updated fixture file.

--- utils/class_scraper.py
+++ utils/class_scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from requests import get
+import datetime
 
 
 class Course:
@@ -41,17 +42,34 @@ class_nums = classes_container.find_all("td", class_='CourseNum')
 
 # list of all Course objects
 classes = []
-
+count = 0
 for i in range(len(class_nums)):
 	class_num_td = class_nums[i]
-	try:
-		class_num = int(class_num_td.text.strip().split(" ")[1])
-	except ValueError: # skip weird transfer class credits (X000T)
-		continue
-	class_name = class_num_td.next_sibling.text.split("(")[0].strip() # remove parenthesized credit amounts from this string
-	class_dept = class_num_td.text.strip().split(" ")[0]
-	course = Course(name=class_name, department=class_dept, number=class_num, pk=i+1)
-	classes.append(course)
+	class_num_tr = class_num_td.parent
+
+	# contains semesters the class was offered
+	class_was_offered = class_num_tr.next_sibling.next_sibling.find("div", class_='CourseWasOffered')
+
+	# assume class is old if no past offering dates found
+	recent_years = [i for i in range(datetime.datetime.now().year, datetime.datetime.now().year-5, -1)]
+
+	if class_was_offered:
+		offered_recently = False
+		for y in recent_years:
+			if str(y) in class_was_offered.text:
+				offered_recently = True
+		if not offered_recently:
+			continue
+
+		try:
+			class_num = int(class_num_td.text.strip().split(" ")[1])
+		except ValueError: # skip weird transfer class credits (X000T)
+			continue
+		class_name = class_num_td.next_sibling.text.split("(")[0].strip() # remove parenthesized credit amounts from this string
+		class_dept = class_num_td.text.strip().split(" ")[0]
+		course = Course(name=class_name, department=class_dept, number=class_num, pk=count+1)
+		classes.append(course)
+		count += 1
 
 with open('../social_match/fixtures/class_data.json', 'w') as file:
 	file.write("[")
