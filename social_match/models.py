a4bf28bbc71dc27661d1e98b850ffde57906550b
Vivian <vwl4ac@virginia.edu>
2019-03-18 15:06:40 -0400

    started to add comments model

--- social_match/models.py
+++ social_match/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.utils import timezone
 import datetime
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -115,3 +116,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.headline
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField()
+    date = models.DateTimeField(default = timezone.now)
