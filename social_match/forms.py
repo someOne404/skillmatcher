94b7736038af89ead693fd53d5527345393437c3
Stephen Shamaiengar <s.shamaiengar@gmail.com>
2019-04-08 00:25:52 -0400

    Fix spacing in forms.py

--- social_match/forms.py
+++ social_match/forms.py
@@ -17,15 +17,15 @@ class EditPostForm(forms.ModelForm):
         fields = {'headline', 'message', 'post_active'}
 
 class CommentPostForm(forms.ModelForm):
-	class Meta:
-		model = Comment
-		fields = ('text',)
-		widgets = {
-			'text': forms.TextInput(attrs={
-				'id': 'comment-text',
-				'required': True,
-			})
-		}
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        widgets = {
+            'text': forms.TextInput(attrs={
+                'id': 'comment-text',
+                'required': True,
+            })
+        }
 
 
 class PostSearchForm(forms.Form):
@@ -35,42 +35,42 @@ class PostSearchForm(forms.Form):
     commented = forms.BooleanField(required=False)
 
 class EditProfileForm(forms.ModelForm):
-  status_active = forms.BooleanField(required=False)
-	class Meta:
-		model = User
-		fields = ('first_name', 'last_name', 'phone',
-				  'class_standing', 'graduation_year',
-				  'majors', 'minors', 'skills',
-				  'interests', 'courses', 'activities')
-		widgets = {
-			'majors': autocomplete.ModelSelect2Multiple(
-				url='social_match:major-autocomplete',
-			),
-			'minors': autocomplete.ModelSelect2Multiple(
-				url='social_match:minor-autocomplete',
-			),
-			'courses': autocomplete.ModelSelect2Multiple(
-				url='social_match:course-autocomplete',
-			),
-			'skills': autocomplete.ModelSelect2Multiple(
-				url='social_match:skill-autocomplete',
-			),
-			'activities': autocomplete.ModelSelect2Multiple(
-				url='social_match:activity-autocomplete',
-			),
-			'interests': autocomplete.ModelSelect2Multiple(
-				url='social_match:interest-autocomplete',
-			)
-		}
+    status_active = forms.BooleanField(required=False)
+    class Meta:
+    model = User
+    fields = ('first_name', 'last_name', 'phone',
+              'class_standing', 'graduation_year',
+              'majors', 'minors', 'skills',
+              'interests', 'courses', 'activities')
+    widgets = {
+        'majors': autocomplete.ModelSelect2Multiple(
+            url='social_match:major-autocomplete',
+        ),
+        'minors': autocomplete.ModelSelect2Multiple(
+            url='social_match:minor-autocomplete',
+        ),
+        'courses': autocomplete.ModelSelect2Multiple(
+            url='social_match:course-autocomplete',
+        ),
+        'skills': autocomplete.ModelSelect2Multiple(
+            url='social_match:skill-autocomplete',
+        ),
+        'activities': autocomplete.ModelSelect2Multiple(
+            url='social_match:activity-autocomplete',
+        ),
+        'interests': autocomplete.ModelSelect2Multiple(
+            url='social_match:interest-autocomplete',
+        )
+    }
 
 class ProfileForm(forms.ModelForm):
-	def __init__(self, *args, **kwargs):
-		super(ProfileForm, self).__init__(*args, **kwargs)
-		for field_name, field in self.fields.items():
-			if field_name in self.initial:
-				field.initial = self.initial[field_name]
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+             if field_name in self.initial:
+                field.initial = self.initial[field_name]
 
-	class Meta:
-		model = User
-		fields = ('first_name', 'last_name', 'phone', 'graduation_year', 'class_standing', 'picture',
-				  'majors', 'minors', 'skills', 'interests', 'courses', 'activities',)
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'phone', 'graduation_year', 'class_standing', 'picture', 
+		  'majors', 'minors', 'skills', 'interests', 'courses', 'activities',)
