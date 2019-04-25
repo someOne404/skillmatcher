6e41e3005c00dbed55fc923297f4200ff60d2ce0
Vivian <vwl4ac@virginia.edu>
2019-04-25 16:36:53 -0400

    ui for profile follow/block list

--- social_match/templatetags/socialmatchtags.py
+++ social_match/templatetags/socialmatchtags.py
@@ -1,5 +1,5 @@
 from django import template
-from friendship.models import Block
+from friendship.models import Block, Follow
 
 register = template.Library()
 
@@ -8,3 +8,13 @@ def blocked_by(user_blocker, user_blocked):
     blocking = Block.objects.blocking(user_blocker)
     is_blocked = user_blocked in blocking
     return is_blocked
+
+@register.simple_tag
+def blocking_list(user):
+    blocking = Block.objects.blocking(user)
+    return blocking
+
+@register.simple_tag
+def following_list(user):
+    following = Follow.objects.following(user)
+    return following
