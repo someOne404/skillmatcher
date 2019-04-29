0b70d51a9f25dcf939414de3dc0f53ea80f3ed2d
Vivian <vwl4ac@virginia.edu>
2019-04-29 01:28:21 -0400

    debug=False

--- lucky13/settings.py
+++ lucky13/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x4!_79l1e_1nuv@(r6&cm+j)bze*849qq6xwhunid%n_b)wh^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
